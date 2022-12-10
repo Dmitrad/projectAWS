da5cc931e3339b18597ecea4340788e7e71d1f33
dzmitry <dradzevich@ziyotek.net>
2022-12-10 08:46:38 -0500

    second

---
+++ environments/userdata.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+sudo echo "<h1> TEAMS2 - IMAGES </h1>" > /var/www/html/index.html
+sudo mkdir /var/www/html/images 
+sudo chmod 777 /var/www/html/images
+sudo echo "IMAGES" > /var/www/html/images/index.html
+
+
+
+
+
+# sudo yum update -y
+# sudo yum install -y httpd
+# sudo systemctl start httpd.service
+# # sudo systemctl enable httpd.service
+# sudo echo "<h1> At $(hostname -f) </h1> IMAGES"> /var/www/html/images/index.html
---
+++ environments/userdata_videos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+sudo echo "<h1> TEAM2 - VIDEO </h1>" > /var/www/html/index.html
+sudo mkdir /var/www/html/videos
+sudo chmod 777 /var/www/html/videos
+sudo echo "VIDEO" > /var/www/html/videos/index.html
+
+
+
+# sudo yum update -y
+# sudo yum install -y httpd
+# sudo systemctl start httpd.service
+# # sudo systemctl enable httpd.service
+# sudo echo "<h1> At $(hostname -f) </h1> Videos"> /var/www/html/videos/index.html
