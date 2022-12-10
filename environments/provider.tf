4a1001f1dda931fc2f08237cfdf015f88c525ef1
dzmitry <dradzevich@ziyotek.net>
2022-12-10 18:57:54 -0500

    www

--- environments/provider.tf
+++ environments/provider.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-1"
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "dzmitry-bucket-002"
-#     key    = "docs/myfile"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "dzmitry-bucket-002"
+    key    = "docs/myfile"
+    region = "us-east-1"
+  }
+}
